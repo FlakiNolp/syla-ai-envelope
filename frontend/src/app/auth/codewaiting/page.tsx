f4e5e030dcecae5c9e4365daff17e9afce779f71
Sh6abrA <47980718+Sh6abrA@users.noreply.github.com>
2024-10-27 12:47:51 +0500

    Add code waiting page

--- frontend/src/app/auth/codewaiting/page.tsx
+++ frontend/src/app/auth/codewaiting/page.tsx
@@ -1,10 +1,12 @@
 import { AuthForm } from "@/features/AuthForm"
+import Link from "next/link";
 
 const AuthPage = () => {
     return (
 
-        <div className={"w-full h-full flex justify-center items-center"}>
+        <div className={"absolute w-full h-full flex flex-col gap-1.5 justify-center items-center"}>
             <h1 className={"text-4xl text-tertiary"}>Вам пришло письмо с кодом на почту!</h1>
+            <Link className={"text-primary"} href={"/auth"}>Go back</Link>
         </div>
 
     )
