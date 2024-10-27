7c3adbd1093b45effccf6b0a75b1972d5d2e2498
Maxim Osetrov <maksimosetrov4@gmail.com>
2024-10-27 11:47:25 +0500

    fix

--- app/src/infrastructure/integrations/rag/envelope.py
+++ app/src/infrastructure/integrations/rag/envelope.py
@@ -9,7 +9,7 @@ from infrastructure.integrations.rag.base import BaseRag
 
 
 class Rag(BaseRag):
-    def __init__(self, host: str, port: int, retry_attempts: int = 3, timeout: int = 15):
+    def __init__(self, host: str, port: int, retry_attempts: int = 3, timeout: int = 20):
         self.host: str = host
         self.port: int = port
         self.retry_attempts: int = retry_attempts
@@ -17,23 +17,18 @@ class Rag(BaseRag):
 
     async def send_requests_with_repeat(self, client: ClientSession, message: Message) -> dict:
         async with client.post(
-            f"http://{self.host}:{self.port}/qa/answer", json={"query": message.text, "tok_k": 2, "top_k_img": 2}, timeout=self.timeout
+            f"http://{self.host}:{self.port}/qa/answer", json={"query": message.text, "tok_k": 2, "top_k_img": 5}, timeout=self.timeout
         ) as response:
             print(response.status)
             if not response.ok:
-                raise
+                return None
             return await response.json()
 
     async def generate_answer(self, message: Message, chat_id: UUID7) -> Message | None:
             async with ClientSession() as client:
-                i = 0
-                while i < self.retry_attempts:
-                    try:
-                        i += 1
-                        response_json = await self.send_requests_with_repeat(client, message)
-                        return Message(
-                            chat_id=chat_id, text=response_json["answer"], documents=response_json["pics"], author=Author.ai
-                        )
-                    except Exception as e:
-                        return None
-                return None
+                response_json = await self.send_requests_with_repeat(client, message)
+                if response_json is None:
+                    return None
+                return Message(
+                    chat_id=chat_id, text=response_json["answer"], documents=response_json["pics"], author=Author.ai
+                )
