d9565b459d63acc7886c1bdebf975944101311c8
Sh6abrA <47980718+Sh6abrA@users.noreply.github.com>
2024-10-27 12:37:26 +0500

    Fix

--- frontend/src/shared/providers/PrivateRoute/PrivateRoute.tsx
+++ frontend/src/shared/providers/PrivateRoute/PrivateRoute.tsx
@@ -1,20 +1,24 @@
 'use client'
 import { useRouter } from 'next/navigation';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 export default function PrivateRoute({ children }) {
     const router = useRouter();
-    const isAuthenticated = localStorage.getItem('access_token');
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
 
     useEffect(() => {
+        // Проверяем, что мы на клиенте, и доступен localStorage
+        const token = typeof window !== 'undefined' ? localStorage.getItem('access_token') : null;
+        setIsAuthenticated(token);
 
-        if (!isAuthenticated) {
+        if (!token) {
             router.push('/auth');
         }
-    }, [isAuthenticated, router]);
+    }, [router]);
 
-    if (!isAuthenticated) {
-        return null; // или загрузочный экран
+    // Пока состояние isAuthenticated не установлено, ничего не рендерим
+    if (isAuthenticated === null) {
+        return null; // Можно добавить компонент загрузки
     }
 
     return <>{children}</>;
